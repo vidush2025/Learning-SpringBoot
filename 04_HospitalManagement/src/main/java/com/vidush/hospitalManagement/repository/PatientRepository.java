081c05f589da8024971ad3942a2764f3302752e8
vidush2025 <vidushworks@gmail.com>
2026-07-23 20:49:22 +0530

    learning JPQL queries

--- 04_HospitalManagement/src/main/java/com/vidush/hospitalManagement/repository/PatientRepository.java
+++ 04_HospitalManagement/src/main/java/com/vidush/hospitalManagement/repository/PatientRepository.java
@@ -1,10 +1,31 @@
 package com.vidush.hospitalManagement.repository;
 
 import com.vidush.hospitalManagement.entity.Patient;
+import com.vidush.hospitalManagement.type.BloodGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+    Patient findByName(String name);
+    Patient findByEmail(String email);
+    List<Patient> findByDobBetween(LocalDate start, LocalDate end);
+
+    @Query("SELECT p FROM Patient p " +
+            "WHERE p.bloodGroup = :bloodGroup"
+    )
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroup bg);
+
+    @Query(
+            "SELECT p.bloodGroup, COUNT(p) " +
+                    "FROM Patient p " +
+                    "GROUP BY p.bloodGroup"
+    )
+    List<Object[]> groupByBloodGroup();
 }
--- 04_HospitalManagement/src/test/java/com/vidush/hospitalManagement/PatientTest.java
+++ 04_HospitalManagement/src/test/java/com/vidush/hospitalManagement/PatientTest.java
@@ -3,10 +3,12 @@ package com.vidush.hospitalManagement;
 import com.vidush.hospitalManagement.entity.Patient;
 import com.vidush.hospitalManagement.repository.PatientRepository;
 import com.vidush.hospitalManagement.service.PatientService;
+import com.vidush.hospitalManagement.type.BloodGroup;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -27,7 +29,21 @@ public class PatientTest {
 
     @Test
     public void testTransactionMethod(){
-        Patient p1 = patientService.getPatient(1L);
-        System.out.println("Patient Found:" + p1);
+//        Patient p1 = patientService.getPatient(1L);
+//        Patient p1 = patientRepository.findByEmail("sneha.gupta@example.com");
+//        List<Patient> patients = patientRepository.findByDobBetween(
+//                LocalDate.parse("1998-01-01"),
+//                LocalDate.parse("2000-12-31")
+//        );
+
+//        List<Patient> patients = patientRepository.findByBloodGroup(BloodGroup.A_POSITIVE);
+
+        List<Object[]> bgList = patientRepository.groupByBloodGroup();
+        for(Object[] bGroup: bgList){
+            System.out.println(bGroup[0] + ": " + bGroup[1]);
+        }
+
+//        System.out.println(patients.size() + " Patients Found:" + patients);
+
     }
 }
